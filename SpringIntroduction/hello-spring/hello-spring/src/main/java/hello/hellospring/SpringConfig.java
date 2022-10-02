56a8c1bca84fa266fb12f29ad54d302f39d3a486
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-10-02 16:20:37 +0900

    pure JDBC

--- SpringIntroduction/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ SpringIntroduction/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,15 +1,26 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 
 //@ComponentScan 방식으로 자동 등록이 아닌 @Configuration을 통하여 Bean을 컨테이너에 직접 등록시켜준다.
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     //아래 bean들을 컨테이너에 등록한다.
     //등록된 memberRepository를 사용하여 MemberService 생성자로 넘겨준다.
@@ -22,6 +33,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        //이 프로젝트의 조건 : 아직 데이터 저장소가 선정되지 않음(가상의 시나리오)
+        //저장소를 임시 저장소인 memoryRepository로 선정한 후 실제 데이터베이스가 선정되었을 때 클래스 하나 바꿈으로서 나머지 코드의 변환없이 사용가능하다.
+        return new JdbcMemberRepository(dataSource);
     }
 }
