2b39cfa99a5248af02f2551450d252b5bd8437ab
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-10-02 14:33:53 +0900

    Member Search

--- SpringIntroduction/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ SpringIntroduction/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 //스프링이 @Controller 어노테이션을 확인하고 MemberController 객체를 가지고 있는다.
 //== 스프링 빈이 관리된다.
@@ -37,5 +40,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+
     }
 }
