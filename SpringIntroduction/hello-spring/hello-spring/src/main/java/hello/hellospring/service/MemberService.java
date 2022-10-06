15490ed4d9a36a02d3084a7b3edb585d9fde41e5
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-10-06 21:57:52 +0900

    time we need AOP

--- SpringIntroduction/hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ SpringIntroduction/hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -28,13 +28,23 @@ public class MemberService {
     //회원 가입
     //wrapper클래스 Long이다.
     public Long join(Member member) {
-        //같은 이름의 중복 회원X
-        //변수 추출하기 : Ctrl + Alt + V, 알아서 반환해줌.
-        //리팩토링 : Ctrl + Alt + Shift + T
-        validateDuplicateMember(member); //중복 회원 검증.
 
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try{
+            //같은 이름의 중복 회원X
+            //변수 추출하기 : Ctrl + Alt + V, 알아서 반환해줌.
+            //리팩토링 : Ctrl + Alt + Shift + T
+            validateDuplicateMember(member); //중복 회원 검증.
+
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs);
+        }
+
 
     }
 
@@ -47,7 +57,14 @@ public class MemberService {
 
     //전체 회원 조회
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs);
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
