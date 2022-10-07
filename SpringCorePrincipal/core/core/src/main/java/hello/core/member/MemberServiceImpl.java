02850b059055edac6de7e70bc941206dfbb30657
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-10-07 16:54:48 +0900

    Member Domain Test

--- SpringCorePrincipal/core/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ SpringCorePrincipal/core/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
 
     //가입을 하고 회원 조회하려면 MemberRepository가 필요하다.
+    //DIP 위반! 추상화도 의존, 구체화도 의존
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
