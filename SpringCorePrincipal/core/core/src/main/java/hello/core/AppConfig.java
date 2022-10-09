b47eb604f940ae8ab3e4022d3d249046a86c9888
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-10-10 05:42:34 +0900

    Change AppConfig into Spring Container

--- SpringCorePrincipal/core/core/src/main/java/hello/core/AppConfig.java
+++ SpringCorePrincipal/core/core/src/main/java/hello/core/AppConfig.java
@@ -9,12 +9,16 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //배우를 지정해주는 기획자 역할을 하는 클래스이다.
 //기획자는 공연 참여자인 구현 객체들을 모두 알아야한다.
 //구성 영역이 담당한다.
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         //이렇게 함으로써 MemberServiceImpl이 더 이상 memberRespository가 MemoryMemberRepository를 참조하지 않도록 한다.
         //즉, 더 이상 MemberServiceImpl이 구현객체에 신경 안쓴다. (DIP 지킴!)
@@ -26,15 +30,18 @@ public class AppConfig {
 
     //리팩터링을 함으로써 MemberRepository의 역할이 드러남.
     //동시에 memberService()와 orderService()에 동시에 사용되던 new MemoryMemberRepository를 중복제거하였다.
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     //리팩터링을 함으로써 DiscountPolicy의 역할이 드러남.
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- SpringCorePrincipal/core/core/src/main/java/hello/core/MemberApp.java
+++ SpringCorePrincipal/core/core/src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,23 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
         //MemberServiceImpl constructor 실행함
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+//        MemberService memberService = appConfig.memberService();
+
+        //ApplicationContext는 스프링 컨테이너로, 모든 spring bean들을 관리한다.
+        //@Configuration이 붙은 AppConfig를 설정(구성)정보로 사용한다.
+        //AppConfig 클래스의 환경정보를 가지고 @Bean 어노테이션이 붙은 메소드들을 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록하여 관리한다.
+        //이렇게 스프링 컨테이너에 등록된 객체를 스프링 빈이라고 한다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //메소드 이름을 첫 번째 인자로 전달하고, 두 번째 인자는 반환 타입 클래스의 정보를 전달한다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
