0942f611969c1741118319850b62304604c76350
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-10-10 06:53:49 +0900

    MemberApp using spring bean

--- corecopy/corecopy/src/main/java/hellocopy/corecopy/AppConfig.java
+++ corecopy/corecopy/src/main/java/hellocopy/corecopy/AppConfig.java
@@ -8,21 +8,28 @@ import hellocopy.corecopy.member.MemberServiceImpl;
 import hellocopy.corecopy.member.MemoryMemberRepository;
 import hellocopy.corecopy.order.OrderService;
 import hellocopy.corecopy.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memoryMemberRepository());
     }
 
+    @Bean
     public MemberRepository memoryMemberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memoryMemberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
--- corecopy/corecopy/src/main/java/hellocopy/corecopy/MemberApp.java
+++ corecopy/corecopy/src/main/java/hellocopy/corecopy/MemberApp.java
@@ -3,16 +3,18 @@ package hellocopy.corecopy;
 import hellocopy.corecopy.member.Grade;
 import hellocopy.corecopy.member.Member;
 import hellocopy.corecopy.member.MemberService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberSerivce = ac.getBean("memberSerivce", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
-        memberService.join(member);
+        memberSerivce.join(member);
 
-        Member findMember = memberService.findMember(1L);
+        Member findMember = memberSerivce.findMember(1L);
         System.out.println("new member = " + member.getName());
         System.out.println("find member = " + findMember.getName());
 
