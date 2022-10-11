a2e9eb4d1ae64cf7b7aa64fdfa6620ea7b6b0da6
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-10-11 16:58:48 +0900

    ConfigurationSingletonTest to see if Spring gurentee singleton

--- SpringCorePrincipal/core/core/src/main/java/hello/core/AppConfig.java
+++ SpringCorePrincipal/core/core/src/main/java/hello/core/AppConfig.java
@@ -12,6 +12,23 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// @Bean memberService --> MemoryMemberRepository()
+// @Bean orderService --> MemoryMemberRepository()
+//싱글톤 깨지는것 아닌가?
+//
+
+//예상
+//call AppConfig.memberService
+//call AppConfig.memberRepository
+//call AppConfig.memberRepository
+//call AppConfig.orderService
+//call AppConfig.memberRepository
+
+//결과
+//call AppConfig.memberService
+//call AppConfig.memberRepository
+//call AppConfig.orderService
+
 //배우를 지정해주는 기획자 역할을 하는 클래스이다.
 //기획자는 공연 참여자인 구현 객체들을 모두 알아야한다.
 //구성 영역이 담당한다.
@@ -20,6 +37,7 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         //이렇게 함으로써 MemberServiceImpl이 더 이상 memberRespository가 MemoryMemberRepository를 참조하지 않도록 한다.
         //즉, 더 이상 MemberServiceImpl이 구현객체에 신경 안쓴다. (DIP 지킴!)
         //생성자 주입이라고 한다.
@@ -32,11 +50,13 @@ public class AppConfig {
     //동시에 memberService()와 orderService()에 동시에 사용되던 new MemoryMemberRepository를 중복제거하였다.
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
---
+++ SpringCorePrincipal/core/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,36 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+    @Test
+    void configurationTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //구체 타입으로 꺼낸 이유는 구체 클래스에 테스트 용도로 만든 메서드를 사용하기 위해서이다.
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService --> memberRepository = " + memberRepository1);
+        System.out.println("orderService --> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+
+
+
+    }
+}
