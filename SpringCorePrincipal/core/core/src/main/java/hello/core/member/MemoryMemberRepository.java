a8e82d13322f34dee7c3552fc0c5571723a0bd7d
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-10-11 22:29:23 +0900

    ConflictingBeanDefinitionException

--- SpringCorePrincipal/core/core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ SpringCorePrincipal/core/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
+@Component //스프링 빈에 등록될 때 앞글자 소문자로 바꾸어 등록됨. memoryMemberRepository로 등록된다.
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
