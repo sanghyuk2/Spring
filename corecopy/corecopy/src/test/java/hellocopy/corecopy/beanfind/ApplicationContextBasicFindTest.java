e74d39269f4949c4b2380bb469efaf19a7c92297
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-10-19 17:16:12 +0900

    findBeanByType

--- corecopy/corecopy/src/test/java/hellocopy/corecopy/beanfind/ApplicationContextBasicFindTest.java
+++ corecopy/corecopy/src/test/java/hellocopy/corecopy/beanfind/ApplicationContextBasicFindTest.java
@@ -1,6 +1,7 @@
 package hellocopy.corecopy.beanfind;
 
 import hellocopy.corecopy.AppConfig;
+import hellocopy.corecopy.member.Member;
 import hellocopy.corecopy.member.MemberService;
 import hellocopy.corecopy.member.MemberServiceImpl;
 import org.junit.jupiter.api.Assertions;
@@ -11,6 +12,11 @@ import
         org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import static org.assertj.core.api.Assertions.*;
 
+/* 스프링 컨테이너에서 스프링을 찾는 가장 기본적인 조회 방법
+ac.getBean(빈이름, 타입)
+ac.getBean(타입)
+*/
+
 class ApplicationContextBasicFindTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
@@ -19,5 +25,13 @@ class ApplicationContextBasicFindTest {
     void findBeanByName() {
         MemberService memberService = ac.getBean("memberService", MemberService.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입만으로 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+
     }
 }
