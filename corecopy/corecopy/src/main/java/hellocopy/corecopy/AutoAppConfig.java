7fc76e4762ebd8e2193c9de2fafbb7542c2bcd65
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-10-20 17:40:00 +0900

    Springboot prevent from making same @Bean name

--- corecopy/corecopy/src/main/java/hellocopy/corecopy/AutoAppConfig.java
+++ corecopy/corecopy/src/main/java/hellocopy/corecopy/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hellocopy.corecopy;
 
+import hellocopy.corecopy.member.MemberRepository;
+import hellocopy.corecopy.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,5 +15,12 @@ import static org.springframework.context.annotation.ComponentScan.*;
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    //수동 빈 등록 vs 자동 빈 등록은 수동 빈 등록이 우선권을 가지나
+    //최근 스프링 부트에서는 이러한 등록으로 인하여 발생가능한 잡기 어려운 버그를 미연에 방지하기 위하여 에러를 발생시킨다.
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 
 }
---
+++ corecopy/corecopy/src/main/java/hellocopy/corecopy/CoreApplication.java
@@ -0,0 +1,14 @@
+package hellocopy.corecopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@Component를 포함하고 있다.
+@SpringBootApplication
+public class CoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+
+}
