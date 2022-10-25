2e68cd3fc18fb31635df93d6d63963876610e65a
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-10-25 23:38:05 +0900

    InitializingBean, DisposableBean

--- corecopy/corecopy/src/main/java/hellocopy/corecopy/lifecycle/NetworkClient.java
+++ corecopy/corecopy/src/main/java/hellocopy/corecopy/lifecycle/NetworkClient.java
@@ -3,16 +3,17 @@ package hellocopy.corecopy.lifecycle;
 /*데이터베이스 커넥션 풀이나, 네트워크 소켓처럼 애플리케이션 시작 시점에 필요한 연결을 미리 해두고, (고객 응답 보다 빠르게 가능)
 애플리케이션 종료 시점에 연결을 모두 종료하는 작업을 진행하려면, 객체의 초기화와 종료 작업이 필요하다. */
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 //외부 네트워크에 미리 연결하는 객체
-public class NetworkClient {
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
+    //생성자는 유지보수 관점에서 객체 생성과 초기화 하는 부분을 명확하게 나누는것이 좋다.
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        //연결 됐을 때 부를 수 있다고 가정
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -24,6 +25,7 @@ public class NetworkClient {
         System.out.println("connect: " + url);
     }
 
+    //연결 됐을 때 부를 수 있다고 가정
     public void call(String message) {
         System.out.println("call: " + url + " message = " + message);
     }
@@ -32,4 +34,17 @@ public class NetworkClient {
     public void disconnect() {
         System.out.println("close: " + url);
     }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
+
+
 }
