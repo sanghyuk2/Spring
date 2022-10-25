227f4873b5fc83dbbf3991889910fe40bf4478a2
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-10-25 22:34:05 +0900

    @Primary

--- corecopy/corecopy/src/main/java/hellocopy/corecopy/discount/RateDiscountPolicy.java
+++ corecopy/corecopy/src/main/java/hellocopy/corecopy/discount/RateDiscountPolicy.java
@@ -3,10 +3,11 @@ package hellocopy.corecopy.discount;
 import hellocopy.corecopy.member.Grade;
 import hellocopy.corecopy.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("mainDiscountPolicy")
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
